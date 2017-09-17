7bfad612bfd309967fe89fd4f744e8aef171a511
hdknr <gmail@hdknr.com>
2017-09-17 03:37:55 +0000

    django tests

---
+++ tests/settings.py
@@ -0,0 +1,23 @@
+SECRET_KEY = 'fake-key'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'bootstrap3',
+    'mymedia',
+    'django_nose',
+]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.sqllite3',
+        'TEST_NAME': 'test.sqlite3',
+        # 'NAME': ':memory:',
+    }
+}
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
