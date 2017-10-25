3cee30e52951f4b1c3241ef1e4c0cf0b7a19340b
hdknr <gmail@hdknr.com>
2017-10-25 18:04:59 +0900

    update ImageMeta info for MediaFile

--- mymedia/tasks.py
+++ mymedia/tasks.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from . import models
 from mytaggit.utils import Kakasi
@@ -8,7 +8,7 @@ import re
 
 
 @receiver(pre_save, sender=models.MediaFile)
-def on_medafile_saving(sender, instance=None, **kwargs):
+def on_mediafile_saving(sender, instance=None, **kwargs):
     if instance:
         name, ext = os.path.splitext(instance.data.name)
         instance.title = instance.title or os.path.basename(name)
@@ -17,3 +17,8 @@ def on_medafile_saving(sender, instance=None, **kwargs):
             instance.filename = re.sub(r'--+', '-', instance.filename)
         instance.media_type = models.MediaType.objects.get_for(
             instance.data.name)
+
+
+@receiver(post_save, sender=models.MediaFile)
+def on_mediafile_saved(sender, instance=None, **kwargs):
+    instance and instance.update_meta()
