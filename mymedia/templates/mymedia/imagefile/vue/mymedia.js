a4846244c2ff4b9e2c0d09acaf7a7081da28ba12
hdknr <gmail@hdknr.com>
2017-12-07 13:58:28 +0900

    toggle button

--- mymedia/templates/mymedia/imagefile/vue/mymedia.js
+++ mymedia/templates/mymedia/imagefile/vue/mymedia.js
@@ -1,6 +1,7 @@
 {% include 'mymedia/imagefile/vue/api.js' %}
 {% include 'mymedia/imagefile/vue/text.js' %}
 {% include 'mymedia/imagefile/vue/tags.js' %}
+{% include 'mymedia/imagefile/vue/toggle.js' %}
 {% include 'mymedia/imagefile/vue/mediafile.js' %}
 {% include 'mymedia/imagefile/vue/dropload.js' %}
 {% include 'mymedia/imagefile/vue/gallery.js' %}
